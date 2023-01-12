bc0256648164b22082b870231c50c5b6438be795
Franco Pari <francopari@Francos-MacBook-Pro.local>
2023-01-11 22:37:12 -0500

    form update

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
           <form
             target="_blank"
             onSubmit={onSubmit}
-            action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+            action="https://formsubmit.co/66db4ae29477e5a46506237d39704dee"
             method="POST"
           >
             <input
